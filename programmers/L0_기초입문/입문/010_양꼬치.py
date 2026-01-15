46e4bcb035dc81e8f1b3c94cde713e3438a16a8d
gorhkdwj <gorhkdwj@gmail.com>
2026-01-16 08:43:24 +0900

    [프로그래머스] L0 - 양꼬치 (#10)

--- programmers/L0_기초입문/입문/010_양꼬치.py
+++ programmers/L0_기초입문/입문/010_양꼬치.py
@@ -3,14 +3,13 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120830
 # 알고리즘: 기초
 # 작성자: 김재천
-# 작성일: 2026. 01. 16. 08:14:48
+# 작성일: 2026. 01. 16. 08:43:24
 
 def solution(n, k):
     answer = 0
     x = n * 12000
-    if n >= 10 :
-        if k >= n//10:
-            k = k-(n//10)
+    if n >= 10 and k >= n//10:
+        k = k-(n//10)
     y = k * 2000
     answer = x + y
     return answer
