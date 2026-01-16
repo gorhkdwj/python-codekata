1bc50b3c348c8f3e2f982da00bc4f70248d56011
gorhkdwj <gorhkdwj@gmail.com>
2026-01-16 09:50:45 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,15 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 김재천
+# 작성일: 2026. 01. 16. 09:50:45
+
+# n//7이 1 이상
+def solution(n):
+    if n!=0:
+        if n%7==0:
+            answer = n//7
+        elif n%7!=0:
+            answer = (n//7)+1
+    return answer
