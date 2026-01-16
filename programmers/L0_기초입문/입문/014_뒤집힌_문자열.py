5b5ae39d97f417004b10641365b1a927ed09cef8
gorhkdwj <gorhkdwj@gmail.com>
2026-01-16 09:28:24 +0900

    [프로그래머스] L0 - 뒤집힌 문자열 (#14)

--- programmers/L0_기초입문/입문/014_뒤집힌_문자열.py
+++ programmers/L0_기초입문/입문/014_뒤집힌_문자열.py
@@ -3,11 +3,14 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120822
 # 알고리즘: 기초
 # 작성자: 김재천
-# 작성일: 2026. 01. 16. 09:27:39
+# 작성일: 2026. 01. 16. 09:28:24
+
+# def solution(my_string):
+#     answer = ''
+#     lst = list(my_string)
+#     reversed_lst = lst[::-1]
+#     answer="".join(reversed_lst)
+#     return answer
 
 def solution(my_string):
-    answer = ''
-    lst = list(my_string)
-    reversed_lst = lst[::-1]
-    answer="".join(reversed_lst)
-    return answer
+    return my_string[::-1]
