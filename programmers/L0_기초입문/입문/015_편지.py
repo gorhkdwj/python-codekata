d7a712789f33c815f06735e6e4ea5561e5158239
gorhkdwj <gorhkdwj@gmail.com>
2026-01-16 09:38:10 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,15 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 김재천
+# 작성일: 2026. 01. 16. 09:38:10
+
+# len = 문자열의 길이
+# len*2 = answer
+
+def solution(message):
+    answer = 0
+    len_mes = len(message)
+    answer = len_mes*2
+    return answer
