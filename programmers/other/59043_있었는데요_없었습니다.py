3aff6ca8b1e8812a59c7b9e348af393b1351a83f
gorhkdwj <gorhkdwj@gmail.com>
2026-01-18 14:17:07 +0900

    [프로그래머스] 있었는데요 없었습니다 풀이 제출
    - 문제 ID: 59043
    - 난이도: unknown
    - 제출일: 2026. 01. 18. 14:17:06
    - 작성자: 김재천

--- programmers/other/59043_있었는데요_없었습니다.py
+++ programmers/other/59043_있었는데요_없었습니다.py
@@ -2,16 +2,16 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59043
 # 작성자: 김재천
-# 작성일: 2026. 01. 18. 13:53:27
+# 작성일: 2026. 01. 18. 14:17:06
 
 -- 보호 시작일>압영일
 -- animal_id, name 조회
--- i.datetime desc
+-- i.datetime
 SELECT
     i.animal_id,
     i.name
 from animal_ins as i
-inner join animal_outs as o
+left join animal_outs as o
 on i.animal_id = o.animal_id
 where i.datetime>o.datetime
-order by i.datetime 
+order by i.datetime
