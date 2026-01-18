1dcc4f3c6dd7ec6edf9eae77782e62e98c047942
gorhkdwj <gorhkdwj@gmail.com>
2026-01-18 14:50:15 +0900

    [프로그래머스] 오랜 기간 보호한 동물(2) 풀이 제출
    - 문제 ID: 59411
    - 난이도: unknown
    - 제출일: 2026. 01. 18. 14:50:14
    - 작성자: 김재천

--- programmers/other/59411_오랜_기간_보호한_동물(2).py
+++ programmers/other/59411_오랜_기간_보호한_동물(2).py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59411
 # 작성자: 김재천
-# 작성일: 2026. 01. 18. 14:45:15
+# 작성일: 2026. 01. 18. 14:50:14
 
 -- o.datetime - i.datetime 상위 2마리
 # 1번 풀이 : order by 후 limit 2
@@ -18,7 +18,6 @@
 # limit 2;
 
 # 2번 풀이 rank 사용
-
 select
     a.animal_id,
     a.name
