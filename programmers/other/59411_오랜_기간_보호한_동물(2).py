4e49f22d631bc946a7e43743b79f598c881b5640
gorhkdwj <gorhkdwj@gmail.com>
2026-01-18 14:45:16 +0900

    [프로그래머스] 오랜 기간 보호한 동물(2) 풀이 제출
    - 문제 ID: 59411
    - 난이도: unknown
    - 제출일: 2026. 01. 18. 14:45:15
    - 작성자: 김재천

--- programmers/other/59411_오랜_기간_보호한_동물(2).py
+++ programmers/other/59411_오랜_기간_보호한_동물(2).py
@@ -2,16 +2,35 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59411
 # 작성자: 김재천
-# 작성일: 2026. 01. 18. 14:36:54
+# 작성일: 2026. 01. 18. 14:45:15
 
 -- o.datetime - i.datetime 상위 2마리
-SELECT
-    i.animal_id,
-    i.name
+# 1번 풀이 : order by 후 limit 2
+# SELECT
+#     i.animal_id,
+#     i.name
+# from
+#     animal_ins as i
+#     inner join animal_outs as o
+#     on i.animal_id = o.animal_id
+# order by
+#     o.datetime - i.datetime desc
+# limit 2;
+
+# 2번 풀이 rank 사용
+
+select
+    a.animal_id,
+    a.name
 from
-    animal_ins as i
-    inner join animal_outs as o
-    on i.animal_id = o.animal_id
-order by
-    o.datetime - i.datetime desc
-limit 2;
+    (
+    select
+        i.animal_id,
+        i.name,
+        rank() over (order by (o.datetime-i.datetime) desc) as ranking
+    from
+        animal_ins as i
+        inner join animal_outs as o
+        on i.animal_id=o.animal_id
+    ) as a
+where ranking <= 2
