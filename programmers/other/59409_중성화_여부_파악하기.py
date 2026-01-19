f193f7279f8cf3023e769caceade9e7328d285b9
gorhkdwj <gorhkdwj@gmail.com>
2026-01-19 11:39:20 +0900

    [프로그래머스] 중성화 여부 파악하기 풀이 제출
    - 문제 ID: 59409
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 11:39:19
    - 작성자: 김재천

--- programmers/other/59409_중성화_여부_파악하기.py
+++ programmers/other/59409_중성화_여부_파악하기.py
@@ -2,18 +2,27 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59409
 # 작성자: 김재천
-# 작성일: 2026. 01. 19. 11:36:19
+# 작성일: 2026. 01. 19. 11:39:19
 
 -- 코드를 입력하세요
-SELECT
-    animal_id,
-    name,
-    case when sex_upon_intake like 'neutered%'
-              or sex_upon_intake like 'spayed%'
-              then 'O'
-    else 'X'
-    end as "중성화"
-from
-    animal_ins
-order by
-    animal_id asc
+# SELECT
+#     animal_id,
+#     name,
+#     case when sex_upon_intake like 'neutered%'
+#               or sex_upon_intake like 'spayed%'
+#               then 'O'
+#     else 'X'
+#     end as "중성화"
+# from
+#     animal_ins
+# order by
+#     animal_id asc
+
+SELECT ANIMAL_ID,
+       NAME,
+       CASE
+           WHEN SEX_UPON_INTAKE REGEXP 'Neutered|Spayed' THEN 'O'
+           ELSE 'X'
+       END AS 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
