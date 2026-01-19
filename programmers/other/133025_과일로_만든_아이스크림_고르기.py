07b23240a897a7beaac3f763284a533e2961af3e
gorhkdwj <gorhkdwj@gmail.com>
2026-01-19 10:38:00 +0900

    [프로그래머스] 과일로 만든 아이스크림 고르기 풀이 제출
    - 문제 ID: 133025
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 10:37:59
    - 작성자: 김재천

--- programmers/other/133025_과일로_만든_아이스크림_고르기.py
+++ programmers/other/133025_과일로_만든_아이스크림_고르기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/133025
 # 작성자: 김재천
-# 작성일: 2026. 01. 19. 10:34:46
+# 작성일: 2026. 01. 19. 10:37:59
 
 SELECT i.flavor
 FROM icecream_info i
